bb773d645a6adecb1fafed53bd2e3f46e3fae282
Yassine_Boul-Hanna <happybiau@gmail.com>
2021-12-06 22:35:31 +0100

    add responsive nav bar

--- OMS/src/app/app-routing.module.ts
+++ OMS/src/app/app-routing.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { FeaturesComponent } from './features/features.component';
+import { DocumentationComponent } from './documentation/documentation.component';
+import { AboutComponent } from './about/about.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
 const routes: Routes = [
   { path:"home",component: HomeComponent },
+  { path:"features",component: FeaturesComponent },
+  { path:"doc",component: DocumentationComponent },
+  { path:"about",component: AboutComponent },
   {path: '',   redirectTo: '/home', pathMatch: 'full'},
   { path: '**', component: PageNotFoundComponent }
 ];
